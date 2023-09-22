59def09f89f3affd02b90b2124bae12fc7b277e4
Ben Johnson <monk.sound.design@gmail.com>
2023-09-22 01:49:17 -0400

    logo name style for screen size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 
@@ -10,7 +10,6 @@ import { scrolltoTop } from "../helpers";
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
-  console.log(toggle);
 
   return (
     <nav
@@ -26,9 +25,11 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Ben Johnson{" "}
-            <span className="sm:block hidden">Full Stack Developer</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Ben &nbsp;
+            <span className="md:block hidden">
+              | &nbsp;Full Stack Developer
+            </span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -45,6 +46,7 @@ const Navbar = () => {
           ))}
         </ul>
 
+        {/* MOBILE MENU */}
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
@@ -57,7 +59,25 @@ const Navbar = () => {
             className={`${
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          ></div>
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <HashLink to={`#${link.id}`}>{link.title}</HashLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* MOBILE MENU */}
         </div>
       </div>
     </nav>
