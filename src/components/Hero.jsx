4a8149855d9a75721b1067a46e495a8878b99fad
Ben Johnson <monk.sound.design@gmail.com>
2023-09-22 12:30:20 -0400

    hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { motion } from "framer-motion";
 import { styles } from "../style";
 import { ComputersCanvas } from "./canvas";
@@ -8,9 +9,20 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col hustify-center items-center mt-5">
+        <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Ben.</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I design, develop, and deploy clean, lightweight web applications
+            and software built to make life better and more fun for{" "}
+            <span className="text-[#915eff]">everyone.</span>
+          </p>
         </div>
       </div>
     </section>
