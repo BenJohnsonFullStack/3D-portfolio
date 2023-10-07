6e6f69e1be9f45fdbf7b362d93da1fdacfe2f249
Ben Johnson <monk.sound.design@gmail.com>
2023-10-07 17:00:19 -0400

    about me text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,15 +41,27 @@ const About = () => {
         </h2>
       </motion.div>
 
-      <motion.p
+      <motion.div
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
-        nesciunt perspiciatis doloribus provident, adipisci error, dolorem
-        consequuntur recusandae illo sed veritatis nisi reiciendis illum
-        pariatur porro! Doloribus, porro distinctio? Doloremque.
-      </motion.p>
+        <p>
+          I believe that technology should be built to serve, entertain, and
+          inspire people, and that creating intuitive, responsive solutions
+          always begins with identifying who we are building a solution for:
+        </p>
+        <ul className="px-16 py-8 list-disc">
+          <li>Where are the opportunities?</li>
+          <li>What does the future look like?</li>
+          <li>How much is too much?</li>
+          <li>Can EVERYONE use it?</li>
+        </ul>
+        <p>
+          As a developer, my job is to be adaptable, forward-thinking,
+          minimalist, and in touch with the always expanding needs of people and
+          industries from diverse backgrounds.
+        </p>
+      </motion.div>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import { motion } from "framer-motion";
+
 import { styles } from "../style";
-import { ComputersCanvas } from "./canvas";
-import { HashLink } from "react-router-hash-link";
 
 const Hero = () => {
   return (
@@ -25,24 +23,6 @@ const Hero = () => {
             <span className="text-[#915eff]">everyone.</span>
           </p>
         </div>
-      </div>
-      <ComputersCanvas />
-      <div className="absolute bottom-6 w-full flex justify-center items-center">
-        <HashLink to="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
-            />
-          </div>
-        </HashLink>
       </div>
     </section>
   );
