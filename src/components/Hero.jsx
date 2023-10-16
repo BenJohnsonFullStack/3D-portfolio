6fb3a422b4aad63456a77a2ae8701b8542361b68
Ben Johnson <monk.sound.design@gmail.com>
2023-10-16 01:47:56 -0400

    disable pointer events on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { AvatarCanvas } from "./canvas";
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className="h-full">
+      <div className="h-full pointer-events-none">
         <div
           className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
         >
