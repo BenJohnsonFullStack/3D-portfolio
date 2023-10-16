5f9e5dbaba46b31af8b520a3047c3c5d545a617d
Ben Johnson <monk.sound.design@gmail.com>
2023-10-16 12:27:07 -0400

    file cleanup

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -8,25 +7,20 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 // technologies
-import bash from "./tech/bash.png";
-import css from "./tech/css.png";
+
 import express from "./tech/express.png";
 import figma from "./tech/figma.png";
 import framerMotion from "./tech/framer-motion.png";
 import git from "./tech/git.png";
 import heroku from "./tech/heroku.png";
-import html from "./tech/html.png";
 import illustrator from "./tech/illustrator.png";
-import javascript from "./tech/javascript.png";
 import jest from "./tech/jest.png";
 import nodejs from "./tech/nodejs.png";
 import postgres from "./tech/postgres.png";
-import postman from "./tech/postman.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import threejs from "./tech/threejs.png";
-import vscode from "./tech/vscode.png";
 
 // work experience
 import lobi from "./company/lobi.png";
@@ -48,7 +42,6 @@ import sherri from "./sherri.webp";
 import esraa from "./esraa.jpg";
 
 export {
-  logo,
   backend,
   creator,
   mobile,
@@ -56,25 +49,19 @@ export {
   github,
   menu,
   close,
-  bash,
-  css,
   express,
   figma,
   framerMotion,
   git,
   heroku,
-  html,
   illustrator,
-  javascript,
   jest,
   nodejs,
   postgres,
-  postman,
   reactjs,
   redux,
   tailwind,
   threejs,
-  vscode,
   lobi,
   chilis,
   gardenom,
